1ec2315352800cbebad65a0be38238d1ffe32c62
Abhishek Ranjan <abhisek.214099@gmail.com>
2026-08-23 18:06:43 +0530

    build the authenticate middleware

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const authenticate = async (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).json({
+        message: "No User Token Found",
+      });
+    }
+  try {
+    const result = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(result.userId);
+    if (!user) {
+      return res.status(401).json({
+        message: "User no longer exists",
+      });
+    }
+    req.user = user;
+    next();
+
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid or expired session",
+    });
+  }
+};
